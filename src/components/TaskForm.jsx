62a4904e922c8b2b0abd3d1b29f1483fcefd6508
Nicolas Ferraz <nicolaspaduaferraz54321@gmail.com>
2025-05-14 19:48:26 -0300

    refactor: add props to control input and submit in TaskForm and AddTaskInput

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,32 @@
 import styles from './TaskForm.module.css'
 
+import { useState } from 'react'
+
 import AddTaskInput from './AddTaskInput'
 
 export default function TaskForm() {
+
+  const [newTask, setNewTask] = useState('')
+
+  const handleOnChange = (e) => {
+    setNewTask(e.target.value)
+    console.log(newTask)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Adiciona a task
+    console.log(newTask)
+    setNewTask('') // Limpa o input após o submit
+  }
+
   return (
     <>
-      <form>
-        <AddTaskInput />
-      </form>
+      <AddTaskInput
+        value={newTask}
+        onChange={handleOnChange}
+        onSubmit={handleSubmit}
+      />
     </>
   )
 }
--- src/components/addTaskInput.jsx
+++ src/components/addTaskInput.jsx
@@ -3,17 +3,20 @@ import styles from './AddTaskInput.module.css'
 // Import react icons
 import { GoPlus } from "react-icons/go";
 
-export default function AddTaskInput() {
+export default function AddTaskInput({ value, onChange, onSubmit }) {
+
   return (
-    <div className={styles.addTask}>
+    <form className={styles.addTask} onSubmit={onSubmit}>
       <input
-      className={styles.addTaskInput}
+        className={styles.addTaskInput}
         type="text"
+        value={value}
+        onChange={onChange}
         placeholder="Digite seu todo"
       />
       <button className={styles.addButton} type="submit">
         <GoPlus />
       </button>
-    </div>
+    </form>
   )
 }
