3941955efeb3dcac8e1f8c6a46ad8060007d94f7
Nicolas Ferraz <nicolaspaduaferraz54321@gmail.com>
2025-05-15 18:06:30 -0300

    fix: add onSubmit prop to handle task form submission

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -24,6 +24,7 @@ export default function TaskForm() {
         <AddTaskInput
           taskText={newTask}
           onChange={handleOnChange}
+          onSubmit={handleOnSubmit}
         />
       </form>
     </>
--- src/components/addTaskInput.jsx
+++ src/components/addTaskInput.jsx
@@ -3,7 +3,7 @@ import styles from './AddTaskInput.module.css'
 // Import react icons
 import { GoPlus } from "react-icons/go";
 
-export default function AddTaskInput({ taskText, onChange }) {
+export default function AddTaskInput({ taskText, onChange, onSubmit }) {
   return (
     <form className={styles.addTask} onSubmit={onSubmit}>
       <input
